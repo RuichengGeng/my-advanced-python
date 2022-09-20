5166779020bd1008bd5b229019edacd9b56afc7f
GengRuicheng <ruicheng.geng@hotmail.com>
2022-09-20 21:45:49 +0800

    cross-reference-gc: first commit in cross reference and garbage collection

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # my-advanced-python
 This repo mainly contains some interesting/advanced topics about Python3 in my work/life
+
+# How to config environment
+### tips: strongly encourage users to use pycharm as IDE
+
+- Download anaconda from official web
+- Open new project in pycharm and navigate to *Project -> Python Interpreter* and choose a conda interpreter
+- Open pycharm integrated terminal and run following lines
+  - Open terminal and key in `conda --version` to make sure conda works
+  - `conda install mamba -c conda-forge`
+  - `mamba env update --file environment.yml`
+  - `mamba env update --file environment-dev.yml`
+- FYI, find conda guide:
+  - https://conda.io/projects/conda/en/latest/user-guide/getting-started.html
---
+++ cross-reference-gc/cross-reference-demo.py
@@ -0,0 +1,71 @@
+from memory_profiler import profile
+import weakref
+
+import os, psutil
+
+
+def bytesto(bytes, to, bsize=1024):
+    a = {'k': 1, 'm': 2, 'g': 3, 't': 4, 'p': 5, 'e': 6 }
+    r = float(bytes)
+    return r / (bsize ** a[to])
+
+
+class A(object):
+    def __init__(self, name, parent=None):
+        self.name = name
+        self._parent = weakref.ref(parent) if parent else parent
+        # self._parent = parent
+        self.children = set()
+        self.workload = ' ' * 128 * 1024 * 1024
+
+    @property
+    def parent(self):
+        if not self._parent:
+            return self._parent
+        _parent = self._parent()
+        if _parent:
+            return _parent
+        else:
+            raise LookupError("Parent was destroyed")
+
+    def __del__(self):
+        print("delete", self.name)
+
+
+class B(object):
+    def __init__(self, name, parent=None):
+        self.name = name
+        self._parent = parent
+        self.children = set()
+        self.workload = ' ' * 128 * 1024 * 1024
+
+    @property
+    def parent(self):
+        if not self._parent:
+            return self._parent
+        _parent = self._parent()
+        if _parent:
+            return _parent
+        else:
+            raise LookupError("Parent was destroyed")
+
+    def __del__(self):
+        print("delete", self.name)
+
+
+@profile
+def main():
+    for _ in range(10):
+        a = A(name=1)
+        a.children.add(A(name=2, parent=a))
+    for _ in range(10):
+        b = B(name=1)
+        b.children.add(B(name=2, parent=b))
+
+    print("end")
+    process = psutil.Process(os.getpid())
+    print(f"memory usage {bytesto(process.memory_info().rss, 'g')} G")  # in Gb
+
+
+if __name__ == "__main__":
+    main()
