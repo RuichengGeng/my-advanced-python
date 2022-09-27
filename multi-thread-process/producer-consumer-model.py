e81b7f3ac52b0306a924267543c09e22e17a0e62
Ruicheng Geng <48621842+RuichengGeng@users.noreply.github.com>
2022-09-27 10:57:14 +0800

    [multi-tread] mutl-thread demos

---
+++ multi-thread-process/producer-consumer-model.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 23:21:10 2021
+
+@author: Ruicheng
+
+This is a very good example of how multi-thread is used in real practice. We simply use 2 queues as input of consumer and producer.
+Program simple get input from the queue(if length of queue > 1).
+"""
+import os
+import queue
+import threading
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+
+
+def craw(url):
+    r = requests.get(url)
+    return r.text
+
+
+def parse(html):
+    # class "post-item-title"
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all("a", class_="post-item-title")
+    return [(link["href"], link.get_text()) for link in links]
+
+
+def do_craw(url_queue: queue.Queue, html_queue: queue.Queue):
+    # produce
+    while True:
+        url = url_queue.get()
+        html = craw(url)
+        html_queue.put(html)
+        print(threading.current_thread().name, "Craw {}, url_queue size {}".format(url, url_queue.qsize()))
+        time.sleep(random.randint(1, 2))
+
+
+def do_parse(html_queue: queue.Queue, fout):
+    while True:
+        html = html_queue.get()
+        results = parse(html)
+        for result in results:
+            fout.write(str(result) + "\n")
+        print(threading.current_thread().name, "result {}, url_queue size {}".format(len(result), html_queue.qsize()))
+        time.sleep(random.randint(1, 2))
+
+
+def main():
+    urls = [
+        f"https://www.cnblogs.com/#p{page}"
+        for page in range(1, 11)
+    ]
+
+    url_queue = queue.Queue()
+    html_queue = queue.Queue()
+    for url in urls:
+        url_queue.put(url)
+
+    for i in range(3):
+        t = threading.Thread(target=do_craw, args=(url_queue, html_queue), name="Craw{}".format(i))
+        t.start()
+
+    fout = open('Craw_ProdConsume.txt', "w")
+    for i in range(2):
+        t = threading.Thread(target=do_parse, args=(html_queue, fout), name="parse{}".format(i))
+        t.start()
+    t.join()
+    fout.close()
+
+
+if __name__ == '__main__':
+    print(os.getcwd())
+    main()
---
+++ multi-thread-process/thread-pool.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Ruicheng
+
+Thread pool is just easy to use and easy to control input/output in my mind... Just get familiar how thread pool
+is called is enough...
+"""
+import time
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup
+import requests
+
+
+def craw(url):
+    r = requests.get(url)
+    return r.text
+
+
+def ten_workers():
+    # Please note that there are lots of parameters about thread pool executor, can take a look if optimization is needed
+    urls = [
+        f"https://www.cnblogs.com/#p{page}"
+        for page in range(1, 51)
+    ]
+    with ThreadPoolExecutor(max_workers=10) as pool:
+        htmls = pool.map(craw, urls)
+    for idx, html in enumerate(htmls):
+        print(urls[idx], len(html))
+
+
+def twenty_workers():
+    # Please note that there are lots of parameters about thread pool executor, can take a look if optimization is needed
+    urls = [
+        f"https://www.cnblogs.com/#p{page}"
+        for page in range(51, 101)
+    ]
+    with ThreadPoolExecutor(max_workers=20) as pool:
+        htmls = pool.map(craw, urls)
+    for idx, html in enumerate(htmls):
+        print(urls[idx], len(html))
+
+
+def main():
+    # here you can see 20 workers are faster than 10
+    start = time.time()
+    ten_workers()
+    end = time.time()
+    print("10 worker thread {}".format(end - start))
+
+    start = time.time()
+    twenty_workers()
+    end = time.time()
+    print("20 worker thread {}".format(end - start))
+
+
+if __name__ == "__main__":
+    main()
+
+
