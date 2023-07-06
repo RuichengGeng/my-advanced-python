1f3db3af5613c88170ce2253f287205196ee646f
Ruicheng Geng <48621842+RuichengGeng@users.noreply.github.com>
2023-07-06 11:29:19 +0800

    [NOISSUE] refactor (#6)

--- rest_api/tutorial/quickstart/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Drink(models.Model):
-    name = models.CharField
--- rest_api_dev/drinks/drinks/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Drinks(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
